d14d3e435fdd2367ecbd5059fde7935ed22200f3
Ritika Shrestha <akitirsth@gmail.com>
2021-10-17 13:07:25 +0545

    updated

--- PlantWebApp/PlantWebApp/settings.py
+++ PlantWebApp/PlantWebApp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -169,3 +170,4 @@ SIMPLE_JWT = {
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
 }
+django_heroku.settings(locals())
